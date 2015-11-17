dc50799476773231bc3fb11776e1c738f731fcf9
Justin Thomas <justinthomas@jtwebs.net>
2015-11-17 13:48:37 -0500

    Maybe using radio for velocity. Not tested!

--- src/sample.cpp
+++ src/sample.cpp
@@ -3,6 +3,15 @@
 #include <trajectory/trajectory.h>
 #include <string>
 
+float deadband(float val, float deadband) {
+  if (val > deadband)
+    return val - deadband;
+  else if (val < -deadband)
+    return val + deadband;
+  else
+    return 0.0;
+}
+
 class DemoServices : public MAVManagerServices
 {
   public:
@@ -14,48 +23,47 @@ class DemoServices : public MAVManagerServices
 
     bool traj_active_;
 
-    // Services that typically use the null_tracker also need to set traj_active to false
-    bool motors_cb(mav_manager::Bool::Request &req, mav_manager::Bool::Response &res)
+    bool useRadioForVelocity_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
     {
-      mav->set_motors(req.b);
-      res.success = true;
-      res.message = "Motors activated";
-      traj_active_ = false;
-      return true;
-    }
-    bool eland_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
-    {
-      res.success = mav->eland();
-      res.message = "Emergency Landing";
-      traj_active_ = false;
-      return res.success;
-    }
-    bool estop_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
-    {
-      mav->estop();
-      res.success = true;
-      res.message = "Emergency Stop";
-      traj_active_ = false;
-      return true;
-    }
-
-    bool useRadioForVelocity_cb(mav_manager::Bool::Request &req, mav_manager::Bool::Response &res)
-    {
-      // TODO: Use setDesVel instead
-      //
-      // res.success = mav_.useRadioForVelocity(req.b);
-      res.success = false;
-      if (res.success)
+      if (useRadioForVelocity_ == false)
       {
-        if (req.b)
-          res.message = "Using radio for velocity";
+        if (mav->have_recent_output_data())
+        {
+          res.success = setVelFromRadio();
+          if (res.success)
+          {
+            useRadioForVelocity_ = true;
+            res.message = "Using radio for velocity";
+            last_cb_ = "useRadioForVelocity";
+          }
+          else
+          {
+            res.message = "Couldn't set velocity using the radio.";
+            ROS_WARN("%s", res.message.c_str());
+          }
+        }
         else
-          res.message = "No longer using radio for velocity";
+        {
+          res.success = false;
+          res.message = "No recent radio data. Not using radio for velocity control.";
+          ROS_WARN("%s", res.message.c_str());
+        }
       }
       else
-        res.message = "Failed to transition";
-
-      return res.success;
+      {
+        res.success = mav->hover();
+        if (res.success)
+        {
+          useRadioForVelocity_ = false;
+          res.message = "No longer using radio for velocity.";
+        }
+        else
+        {
+          res.message = "Could not hover. Still using radio for velocity.";
+          ROS_WARN("%s", res.message.c_str());
+        }
+      }
+      return true;
     }
     bool prepTraj_cb(mav_manager::Vec4::Request &req, mav_manager::Vec4::Response &res)
     {
@@ -69,99 +77,169 @@ class DemoServices : public MAVManagerServices
         traj.UpdateGoal(goal);
 
         res.success = mav->goTo(goal.position.x, goal.position.y, goal.position.z, goal.yaw);
-        return res.success;
+
+        last_cb_ = "prepTraj";
       }
       else
-        ROS_WARN("Cannot prep traj. Trajectory not loaded. Error Code: %d", traj.get_error_code());
-
-      return false;
+      {
+        res.success = false;
+        res.message = "Cannot prep traj. Trajectory not loaded. Error Code: " + std::to_string(traj.get_error_code());
+        ROS_WARN("%s", res.message.c_str());
+      }
+      return true;
     }
     bool executeTrajectory_cb(mav_manager::Trigger::Request &req, mav_manager::Trigger::Response &res)
     {
+      // Make sure the trajectory is loaded
       if (traj.isLoaded())
       {
-        // Make sure starting conditions are close enough
-        double vel = (mav->vel()).norm();
-        if (vel < 0.1)
-        {
-          traj.set_start_time();
-          quadrotor_msgs::PositionCommand goal;
-          traj.UpdateGoal(goal);
-          Vec3 des_pos(goal.position.x, goal.position.y, goal.position.z);
+        res.success = false;
+        res.message = "Trajectory not loaded. Cannot start trajectory.";
+        ROS_WARN("%s", res.message.c_str());
+        return true;
+      }
 
-          double e = (mav->pos() - des_pos).norm();
-          if (e < 0.15)
-          {
-            if(mav->setPositionCommand(goal))
-            {
-              traj.set_start_time();
-              traj_active_ = true;
-              res.success = true;
-              return true;
-            }
-            else
-              ROS_WARN("Could not transition to null tracker");
-          }
-          else
-            ROS_WARN("Position error too large to start trajectory");
-        }
-        else
-          ROS_WARN("Velocity too large to start trajectory");
+      // Check the velocity error
+      float vel = (mav->vel()).norm();
+      if (vel > 0.1)
+      {
+        res.success = false;
+        res.message = "Velocity too large to start trajectory";
+        ROS_WARN("%s", res.message.c_str());
+        return true;
+      }
+
+      // Check the position error
+      traj.set_start_time();
+      quadrotor_msgs::PositionCommand goal;
+      traj.UpdateGoal(goal);
+      MAVManager::Vec3 des_pos(goal.position.x, goal.position.y, goal.position.z);
+
+      float e = (mav->pos() - des_pos).norm();
+      if (e > 0.15)
+      {
+        ROS_WARN("Position error too large to start trajectory");
+        res.success = false;
+        res.message = "Position error too large to start trajectory";
+        ROS_WARN("%s", res.message.c_str());
+        return true;
+      }
+
+      if(mav->setPositionCommand(goal))
+      {
+        traj.set_start_time();
+        traj_active_ = true;
+        res.success = true;
+        ROS_INFO("Executing trajectory...");
+        last_cb_ = "executeTrajectory";
+        return true;
       }
       else
-        ROS_WARN("Trajectory not loaded. Cannot start trajectory.");
+      {
+        res.success = false;
+        res.message = "Could not transition to null tracker";
+        ROS_WARN("Could not transition to null tracker");
+        return true;
+      }
 
       return false;
     }
 
-    void odometry_cb(const nav_msgs::Odometry::ConstPtr &msg)
+    DemoServices(std::shared_ptr<MAVManager> m);
+
+  private:
+
+    ros::NodeHandle priv_nh_;
+    bool useRadioForVelocity_;
+    void odometry_cb(const nav_msgs::Odometry::ConstPtr &msg);
+
+    std::map<std::string, uint8_t> services_map;
+
+    bool setVelFromRadio()
     {
-      if (traj_active_)
-      {
-        if ((mav->active_tracker()).compare("std_trackers/NullTracker") == 0)
-        {
-          quadrotor_msgs::PositionCommand goal;
-          traj.UpdateGoal(goal);
-          mav->setPositionCommand(goal);
+      auto radio = mav->radio();
 
-          if (traj.isCompleted())
-          {
-            traj_active_ = !(mav->hover());
-          }
-        }
-        else
-        {
-          traj_active_ = false;
-        }
-      }
-    }
+      // constants
+      float scale = 255.0 / 2.0;
+      float rc_max_v = 1.0;
+      float rc_max_w = 90.0 * M_PI / 180.0;
 
-    DemoServices(std::shared_ptr<MAVManager> m) : MAVManagerServices(m), traj_active_(false)
-    {
-      // The trajectory filename
-      std::string traj_filename;
-      nh_priv_.param("traj_filename", traj_filename, std::string("traj.csv"));
-      ROS_INFO("Using traj: %s", traj_filename.c_str());
-      traj.set_filename(traj_filename);
+      // scale radio
+      float vel[4] = {0, 0, 0, 0};
+      vel[0] = - ((float)radio[0] - scale) / scale * rc_max_v;
+      vel[1] = - ((float)radio[1] - scale) / scale * rc_max_v;
 
-      // Subscribers
-      odom_sub_ = nh_.subscribe("odom", 1, &DemoServices::odometry_cb, this);
+      // Only consider z velocity if the FLT Mode switch is toggled
+      if (radio[5] > 0)
+        vel[2] = ((float)radio[2] - scale) / scale * rc_max_v;
 
-      srvs_.push_back(nh_.advertiseService("useRadioForVelocity", &DemoServices::useRadioForVelocity_cb, this));
-      srvs_.push_back(nh_.advertiseService("prepTraj", &DemoServices::prepTraj_cb, this));
-      srvs_.push_back(nh_.advertiseService("executeTrajectory", &DemoServices::executeTrajectory_cb, this));
+      vel[3] = - ((float)radio[3] - scale) / scale * rc_max_w;
+
+      // Deadbands on velocity
+      float db[4] = {0.1, 0.1, 0.15, 0.03};
+      for (unsigned int i = 0; i < 4; i++) vel[i] = deadband(vel[i], db[i]);
+
+      return mav->setDesVelInWorldFrame(vel[0], vel[1], vel[2], vel[3]);
     }
 };
+
+void DemoServices::odometry_cb(const nav_msgs::Odometry::ConstPtr &msg)
+{
+  auto radio = mav->radio();
+  bool serial = radio[4] > 0;
+
+
+  if (traj_active_)
+  {
+    if (last_cb_ == "executeTrajectory" && mav->active_tracker() == "std_trackers/NullTracker")
+    {
+      quadrotor_msgs::PositionCommand goal;
+      traj.UpdateGoal(goal);
+      mav->setPositionCommand(goal);
+
+      if (traj.isCompleted())
+        traj_active_ = !(mav->hover());
+    }
+    else
+      traj_active_ = false;
+  }
+
+  if (useRadioForVelocity_)
+  {
+    if (last_cb_ == "useRadioForVelocity" && mav->active_tracker() == "std_trackers/VelocityTracker")
+      setVelFromRadio();
+    else
+      useRadioForVelocity_ = false;
+  }
+};
+
+DemoServices::DemoServices(std::shared_ptr<MAVManager> m) :
+  MAVManagerServices(m),
+  traj_active_(false),
+  useRadioForVelocity_(false)
+{
+  // The trajectory filename
+  std::string traj_filename;
+  nh_.param("traj_filename", traj_filename, std::string("traj.csv"));
+  ROS_INFO("Using traj: %s", traj_filename.c_str());
+  traj.set_filename(traj_filename);
+
+  // Subscribers
+  odom_sub_ = priv_nh_.subscribe("odom", 1, &DemoServices::odometry_cb, this);
+
+  srvs_.push_back(nh_.advertiseService("useRadioForVelocity", &DemoServices::useRadioForVelocity_cb, this));
+  srvs_.push_back(nh_.advertiseService("prepTraj", &DemoServices::prepTraj_cb, this));
+  srvs_.push_back(nh_.advertiseService("executeTrajectory", &DemoServices::executeTrajectory_cb, this));
+}
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "manager");
-  ros::NodeHandle nh;
 
   std::shared_ptr<MAVManager> mav = std::make_shared<MAVManager>();
 
   DemoServices mm_srvs(mav);
-  
+
   ros::spin();
 
   return 0;
